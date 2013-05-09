8687bdd7a1e1ac79b6944af64957e39c7a1806d1
noddy2006 <Samuel.Hindmarsh@gmail.com>
2013-05-09 14:30:13 +1200

    Fixed help text for clear

--- LD26/Assets/Scripts/Terminal/Filesystem/Programs/Clear.js
+++ LD26/Assets/Scripts/Terminal/Filesystem/Programs/Clear.js
@@ -12,10 +12,7 @@ public class Clear extends Executable {
 	}
 	
 	function PrintHelpText(terminalSession : TerminalSession){
-		terminalSession.WriteLine("The clear command simply clears the current terminal display.");
-		terminalSession.WriteLine("You can use relative paths or absolute paths. For example, use \"cd etc\" to move from here to the etc directory;");
-		terminalSession.WriteLine("cd ../bin\" to move up one directory, and then to the bin directory; or \"/inv/\" to move to the inv directory located");
-		terminalSession.WriteLine("in the root directory.");
+		terminalSession.WriteLine("Use the clear command to clear the current terminal display.");
 		terminalSession.WriteLine("");
 		terminalSession.WriteLine("Arguments: (none).");
 	}
