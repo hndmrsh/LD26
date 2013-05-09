a17d20581128488d0f271a932c836d9962255dae
noddy2006 <Samuel.Hindmarsh@gmail.com>
2013-05-09 14:38:49 +1200

    Implemented the pwd command

---
+++ LD26/Assets/Scripts/Terminal/Filesystem/Programs/Pwd.js
@@ -0,0 +1,20 @@
+#pragma strict
+
+public class Pwd extends Executable {
+
+  function Pwd(){
+		super("pwd");
+	}
+
+	function Execute(terminalSession : TerminalSession, args : String[]){
+		terminalSession.WriteLine(terminalSession.GetCurrentDirectory().PathToFile());
+		terminalSession.FinishedExecuting();
+	}
+
+	function PrintHelpText(terminalSession : TerminalSession){
+		terminalSession.WriteLine("Use the pwd command to output the directory you are currently in.");
+		terminalSession.WriteLine("");
+		terminalSession.WriteLine("Arguments: (none).");
+	}
+
+}
